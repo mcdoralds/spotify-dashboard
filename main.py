78745ae0b9c8bcbea79452da6554e3352b47fbcb
Stella <stellalimkim@gmail.com>
2023-10-29 16:21:56 -0700

    functioning api requests and basic search testing

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from dotenv import load_dotenv
 import os
 import base64
 import json
-from requests import post
+import requests
+from requests import post, get
 
 ### File Path for .env file
-dotenv_path = r'J:\Documents\Projects\Spotify Dashboard\.env'
+dotenv_path = r'J:\Documents\Projects\Spotify Dashboard\spotify-dashboard.env'
 load_dotenv(dotenv_path) 
 
 # Get CLIENT_ID and CLIENT_SECRET from environment variables from Spotify Dev Dashboard
@@ -34,5 +35,44 @@ def get_token():
     token = json_result["access_token"] # extracts access token from JSON response
     return token
 
+### Function to create headers for auth requests
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+### Function to search artist details by artist name
+def search_by_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"] # parses JSON response
+
+    if len(json_result) ==0:
+        print ("No artist with that name exists")
+        return None
+    
+    return json_result[0]
+    # print(json_result)
+
+### Function to get songs by artist ID
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url,headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 token = get_token()
 # print(token)
+result = search_by_artist(token, "Planet 1999")
+artist_id = result["id"]
+# print(artist_id)
+songs = get_songs_by_artist(token, artist_id)
+# print(songs)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}.{song['name']}")
